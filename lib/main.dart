1970a88289dd71ad3894e9c00de9afd67cc8d1cc
wade <briane668@g,ail.com>
2023-04-17 00:00:08 +0800

    week4 first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -68,6 +70,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    bool isMobile = screenWidth < 600;
+
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -89,103 +94,215 @@ class _MyHomePageState extends State<MyHomePage> {
           ])
         ],
       )),
-      body: Column(
-        children: [
-          Container(
-            margin: const EdgeInsets.symmetric(vertical: 20.0),
-            height: 200.0,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: null,
-              itemBuilder: (context, index) {
-                return Card(
-                  color: Colors.white,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Image.asset(
-                      'assets/women.jpg',
-                      width: 200,
-                    ),
+      body: isMobile ? HomeMobilePage() : HomeWebPage(),
+    );
+  }
+}
+
+class HomeMobilePage extends StatelessWidget {
+  const HomeMobilePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          margin: const EdgeInsets.symmetric(vertical: 20.0),
+          height: 200.0,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: null,
+            itemBuilder: (context, index) {
+              return Card(
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    'assets/women.jpg',
+                    width: 200,
                   ),
-                );
-              },
-            ),
+                ),
+              );
+            },
           ),
-          Expanded(
-              child: Row(
+        ),
+        Expanded(
+          child: Column(
             children: [
+              Text('女裝'),
               Expanded(
-                child: Column(
-                  children: [
-                    Text('女裝'),
-                    Expanded(
-                      child: Container(
-                          margin: const EdgeInsets.symmetric(vertical: 20.0),
-                          height: 200.0,
-                          width: 300,
-                          child: Flexible(
-                            child: ListView.builder(
-                              scrollDirection: Axis.vertical,
-                              itemCount: null,
-                              itemBuilder: (context, index) {
-                                return cardListView(
-                                    "Net 女裝", 200, 'assets/pngwingwomne.png');
-                              },
-                            ),
-                          )),
-                    )
-                  ],
-                ),
-              ),
-              Expanded(
-                child: Column(
-                  children: [
-                    Text('男裝'),
-                    Expanded(
-                      child: Container(
-                          margin: const EdgeInsets.symmetric(vertical: 20.0),
-                          height: 200.0,
-                          width: 300,
-                          child: Flexible(
-                            child: ListView.builder(
-                              scrollDirection: Axis.vertical,
-                              itemCount: null,
-                              itemBuilder: (context, index) {
-                                return cardListView(
-                                    "Net 男裝", 100, 'assets/pngwing.png');
-                              },
-                            ),
-                          )),
-                    )
-                  ],
-                ),
-              ),
-              Expanded(
-                child: Column(
-                  children: [
-                    Text('配件'),
-                    Expanded(
-                      child: Container(
-                        margin: const EdgeInsets.symmetric(vertical: 20.0),
-                        height: 600.0,
-                        width: 300,
-                        child: ListView.builder(
-                          scrollDirection: Axis.vertical,
-                          itemCount: null,
-                          itemBuilder: (context, index) {
-                            return cardListView(
-                                "Net 配件", 150, 'assets/pngwing.png');
-                          },
-                        ),
+                child: Container(
+                    margin: const EdgeInsets.symmetric(vertical: 20.0),
+                    height: 200.0,
+                    width: 300,
+                    child: Flexible(
+                      child: ListView.builder(
+                        scrollDirection: Axis.vertical,
+                        itemCount: null,
+                        itemBuilder: (context, index) {
+                          return cardListView(
+                              "Net 女裝", 200, 'assets/pngwingwomne.png');
+                        },
                       ),
-                    )
-                  ],
-                ),
-              ),
+                    )),
+              )
             ],
-          ))
-        ],
-      ),
+          ),
+        ),
+        Expanded(
+          child: Column(
+            children: [
+              Text('男裝'),
+              Expanded(
+                child: Container(
+                    margin: const EdgeInsets.symmetric(vertical: 20.0),
+                    height: 200.0,
+                    width: 300,
+                    child: Flexible(
+                      child: ListView.builder(
+                        scrollDirection: Axis.vertical,
+                        itemCount: null,
+                        itemBuilder: (context, index) {
+                          return cardListView(
+                              "Net 男裝", 100, 'assets/pngwing.png');
+                        },
+                      ),
+                    )),
+              )
+            ],
+          ),
+        ),
+        Expanded(
+          child: Column(
+            children: [
+              Text('配件'),
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.symmetric(vertical: 20.0),
+                  height: 600.0,
+                  width: 300,
+                  child: ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    itemCount: null,
+                    itemBuilder: (context, index) {
+                      return cardListView("Net 配件", 150, 'assets/pngwing.png');
+                    },
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class HomeWebPage extends StatelessWidget {
+  const HomeWebPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          margin: const EdgeInsets.symmetric(vertical: 20.0),
+          height: 200.0,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: null,
+            itemBuilder: (context, index) {
+              return Card(
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    'assets/women.jpg',
+                    width: 200,
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+        Expanded(
+            child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                children: [
+                  Text('女裝'),
+                  Expanded(
+                    child: Container(
+                        margin: const EdgeInsets.symmetric(vertical: 20.0),
+                        height: 200.0,
+                        width: 300,
+                        child: Flexible(
+                          child: ListView.builder(
+                            scrollDirection: Axis.vertical,
+                            itemCount: null,
+                            itemBuilder: (context, index) {
+                              return cardListView(
+                                  "Net 女裝", 200, 'assets/pngwingwomne.png');
+                            },
+                          ),
+                        )),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Column(
+                children: [
+                  Text('男裝'),
+                  Expanded(
+                    child: Container(
+                        margin: const EdgeInsets.symmetric(vertical: 20.0),
+                        height: 200.0,
+                        width: 300,
+                        child: Flexible(
+                          child: ListView.builder(
+                            scrollDirection: Axis.vertical,
+                            itemCount: null,
+                            itemBuilder: (context, index) {
+                              return cardListView(
+                                  "Net 男裝", 100, 'assets/pngwing.png');
+                            },
+                          ),
+                        )),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Column(
+                children: [
+                  Text('配件'),
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(vertical: 20.0),
+                      height: 600.0,
+                      width: 300,
+                      child: ListView.builder(
+                        scrollDirection: Axis.vertical,
+                        itemCount: null,
+                        itemBuilder: (context, index) {
+                          return cardListView(
+                              "Net 配件", 150, 'assets/pngwing.png');
+                        },
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ))
+      ],
     );
   }
 }
@@ -605,4 +722,226 @@ class DetailPagestate extends State<DetailPage> {
           ),
         ));
   }
+}
+
+List<ClothesItem> generateMockClothesItems(int count, String category) {
+  final random = Random();
+
+  return List<ClothesItem>.generate(count, (index) {
+    final int id;
+    final String name;
+    final String imageUrl;
+    if (category == "男裝") {
+      id = 100 + random.nextInt(99);
+      name = getRandomClothingName();
+      imageUrl = "assets/images/img_clothes_men.png";
+    } else if (category == "女裝") {
+      id = 200 + random.nextInt(99);
+      name = getRandomClothingName();
+      imageUrl = "assets/images/img_clothes_women.png";
+    } else if (category == "配件") {
+      id = 300 + random.nextInt(99);
+      name = getRandomAssesoriesName();
+      imageUrl = "assets/images/img_assesories.png";
+    } else {
+      id = 400 + random.nextInt(99);
+      name = getRandomClothingName();
+      imageUrl = "assets/images/img_top_banner.jpg";
+    }
+
+    final price = "NT\$ ${random.nextInt(10000)}";
+    final colors = getRandomClothingColors();
+    final sizes = getRandomClothingSizes();
+    const colorHint = "實品顏色依單品照為主";
+    final material = getRandomClothingMaterial();
+    final thickLevel = getRandomClothingThick();
+    final flexibility = getRandonClothingFlexibility();
+    final materialOrigin = getRandomClothingOrigin();
+    final processOrigin = getRandomClothingOrigin();
+
+    return ClothesItem(
+        id: id,
+        name: name,
+        imageUrl: imageUrl,
+        price: price,
+        colors: colors,
+        sizes: sizes,
+        colorHint: colorHint,
+        material: material,
+        thickLevel: thickLevel,
+        flexibility: flexibility,
+        materialOrigin: materialOrigin,
+        processOrigin: processOrigin);
+  });
+}
+
+class ClothesItem {
+  final int id;
+  final String name;
+  final String imageUrl;
+  final String price;
+  final List<Color> colors;
+  final List<String> sizes;
+  final String colorHint;
+  final String material;
+  final String thickLevel;
+  final String materialOrigin;
+  final String processOrigin;
+  final String flexibility;
+
+  ClothesItem(
+      {required this.id,
+      required this.name,
+      required this.imageUrl,
+      required this.price,
+      required this.colors,
+      required this.sizes,
+      required this.colorHint,
+      required this.material,
+      required this.thickLevel,
+      required this.materialOrigin,
+      required this.processOrigin,
+      required this.flexibility});
+}
+
+String getRandomClothingName() {
+  final clothingNames = [
+    'T-Shirt',
+    '牛仔褲',
+    '毛衣',
+    '連帽衫',
+    '夾克',
+    '短袖',
+    '半身裙',
+    '連身裙',
+    '瑜珈褲',
+    '外套',
+    '襯衫',
+    '皮衣',
+    '羽絨衣',
+    '皮草大衣',
+    '羊毛大衣',
+  ];
+
+  final random = Random();
+  int randomIndex = random.nextInt(clothingNames.length);
+
+  return clothingNames[randomIndex];
+}
+
+List<Color> getRandomClothingColors() {
+  final clothingColors = [
+    Colors.black,
+    Colors.white,
+    Colors.red,
+    Colors.blue,
+    Colors.green,
+    Colors.yellow,
+    Colors.orange,
+    Colors.pink,
+    Colors.purple,
+    Colors.brown,
+  ];
+
+  final random = Random();
+  int sampleSize = random.nextInt(5) + 1;
+
+  return clothingColors.take(sampleSize).toList();
+}
+
+List<String> getRandomClothingSizes() {
+  final clothingSizes = [
+    'S',
+    'M',
+    'L',
+    'XL',
+  ];
+
+  final random = Random();
+  int sampleSize = random.nextInt(4) + 1;
+
+  return clothingSizes.take(sampleSize).toList();
+}
+
+String getRandomClothingMaterial() {
+  final clothingMaterial = [
+    '棉 100%',
+    '棉 80% / 聚酯纖維 20%',
+    '棉 70% / 聚酯纖維 30%',
+    '棉 60% / 聚酯纖維 40%',
+    '聚酯纖維 100%',
+    '聚酯纖維 80% / 棉 20%',
+    '天然蠶絲 100%',
+    '羊毛 100%',
+    '鵝絨 100%',
+  ];
+
+  final random = Random();
+  int randomIndex = random.nextInt(clothingMaterial.length);
+
+  return clothingMaterial[randomIndex];
+}
+
+String getRandomClothingThick() {
+  final clothingThick = [
+    '薄',
+    '厚',
+  ];
+
+  final random = Random();
+  int randomIndex = random.nextInt(clothingThick.length);
+
+  return clothingThick[randomIndex];
+}
+
+String getRandonClothingFlexibility() {
+  final clothingFlexcibility = [
+    '有',
+    '無',
+  ];
+
+  final random = Random();
+  int randomIndex = random.nextInt(clothingFlexcibility.length);
+
+  return clothingFlexcibility[randomIndex];
+}
+
+String getRandomClothingOrigin() {
+  final clothingOrigin = [
+    "台灣",
+    "日本",
+    "韓國",
+    "美國",
+    "英國",
+    "中國",
+    "菲律賓",
+    "印尼",
+    "越南",
+    "泰國",
+  ];
+
+  final random = Random();
+  int randomIndex = random.nextInt(clothingOrigin.length);
+
+  return clothingOrigin[randomIndex];
+}
+
+String getRandomAssesoriesName() {
+  final assesoriesNames = [
+    '皮帶',
+    '手套',
+    '帽子',
+    '襪子',
+    '圍巾',
+    '鞋子',
+    '皮包',
+    '手提包',
+    '領帶',
+    '領帶夾',
+  ];
+
+  final random = Random();
+  int randomIndex = random.nextInt(assesoriesNames.length);
+
+  return assesoriesNames[randomIndex];
 }
