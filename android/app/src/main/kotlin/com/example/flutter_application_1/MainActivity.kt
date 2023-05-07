9f6c973138736655bf1b82307d158c0833e3fdcd
wade <briane668@g,ail.com>
2023-05-08 00:28:23 +0800

    week5

--- android/app/src/main/kotlin/com/example/flutter_application_1/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_application_1/MainActivity.kt
@@ -5,14 +5,37 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
+import com.example.stylish.tappay.CardInfo
+import com.example.stylish.tappay.TapPayHelper
+import com.example.stylish.tappay.TapPayHelperImpl
+
 class MainActivity : FlutterActivity() {
 
+    private val tapPayHelper: TapPayHelper by lazy {
+        TapPayHelperImpl(this, applicationContext)
+    }
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
             when (call.method) {
-
+                "getTappayPrime" -> {
+                    val cardInfo = CardInfo(
+                        cardNumber = call.argument<String>("cardNumber"),
+                        dueMonth = call.argument<String>("dueMonth"),
+                        dueYear = call.argument<String>("dueYear"),
+                        ccv = call.argument<String>("ccv")
+                    )
+                    tapPayHelper.checkout(
+                        cardInfo = cardInfo,
+                        onSuccess = { prime ->
+                            result.success(prime)
+                        },
+                        onFailure = { message ->
+                            result.error("Error", message, null)
+                        }
+                    )
+                }
 
                 else -> {
                     result.notImplemented()
@@ -25,3 +48,4 @@ class MainActivity : FlutterActivity() {
         private val CHANNEL = "com.stylish.app/tappay"
     }
 }
+
