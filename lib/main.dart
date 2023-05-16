aca4bbea7547bf47d37448c365703903013150ea
sfwadelee <wadelee@softchef.com>
2023-05-16 19:34:49 +0800

    andorid arkit

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,126 @@
-import 'dart:math';
-
+import 'package:ar_flutter_plugin/managers/ar_location_manager.dart';
+import 'package:ar_flutter_plugin/managers/ar_session_manager.dart';
+import 'package:ar_flutter_plugin/managers/ar_object_manager.dart';
+import 'package:ar_flutter_plugin/managers/ar_anchor_manager.dart';
 import 'package:flutter/material.dart';
-import 'package:dio/dio.dart';
-import 'package:flutter/services.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'package:cached_network_image/cached_network_image.dart';
+import 'package:ar_flutter_plugin/ar_flutter_plugin.dart';
+import 'package:ar_flutter_plugin/datatypes/config_planedetection.dart';
+import 'package:ar_flutter_plugin/managers/ar_location_manager.dart';
+import 'package:ar_flutter_plugin/managers/ar_session_manager.dart';
+import 'package:ar_flutter_plugin/managers/ar_object_manager.dart';
+import 'package:ar_flutter_plugin/managers/ar_anchor_manager.dart';
 
-import 'package:flutter/material.dart';
-import 'package:arkit_plugin/arkit_plugin.dart';
-import 'package:vector_math/vector_math_64.dart';
+import 'package:ar_flutter_plugin/ar_flutter_plugin.dart';
+import 'package:ar_flutter_plugin/datatypes/config_planedetection.dart';
 
 void main() => runApp(MaterialApp(home: MyApp()));
 
 class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  late ARKitController arkitController;
+  ARSessionManager? arSessionManager;
+  ARObjectManager? arObjectManager;
+  bool _showFeaturePoints = false;
+  bool _showPlanes = false;
+  bool _showWorldOrigin = false;
+  bool _showAnimatedGuide = true;
+  String _planeTexturePath = "Images/triangle.png";
+  bool _handleTaps = false;
 
   @override
   void dispose() {
-    arkitController.dispose();
     super.dispose();
+    arSessionManager!.dispose();
   }
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(title: const Text('ARKit in Flutter')),
-      body: ARKitSceneView(onARKitViewCreated: onARKitViewCreated));
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Debug Options'),
+        ),
+        body: Container(
+            child: Stack(children: [
+          ARView(
+            onARViewCreated: onARViewCreated,
+            planeDetectionConfig: PlaneDetectionConfig.horizontalAndVertical,
+            showPlatformType: true,
+          ),
+          Align(
+            alignment: FractionalOffset.bottomRight,
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.5,
+              color: Color(0xFFFFFFF).withOpacity(0.5),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SwitchListTile(
+                    title: const Text('Feature Points'),
+                    value: _showFeaturePoints,
+                    onChanged: (bool value) {
+                      setState(() {
+                        _showFeaturePoints = value;
+                        updateSessionSettings();
+                      });
+                    },
+                  ),
+                  SwitchListTile(
+                    title: const Text('Planes'),
+                    value: _showPlanes,
+                    onChanged: (bool value) {
+                      setState(() {
+                        _showPlanes = value;
+                        updateSessionSettings();
+                      });
+                    },
+                  ),
+                  SwitchListTile(
+                    title: const Text('World Origin'),
+                    value: _showWorldOrigin,
+                    onChanged: (bool value) {
+                      setState(() {
+                        _showWorldOrigin = value;
+                        updateSessionSettings();
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ])));
+  }
 
-  void onARKitViewCreated(ARKitController arkitController) {
-    this.arkitController = arkitController;
-    final node = ARKitNode(
-        geometry: ARKitSphere(radius: 0.1), position: Vector3(0, 0, -0.5));
-    this.arkitController.add(node);
+  void onARViewCreated(
+      ARSessionManager arSessionManager,
+      ARObjectManager arObjectManager,
+      ARAnchorManager arAnchorManager,
+      ARLocationManager arLocationManager) {
+    this.arSessionManager = arSessionManager;
+    this.arObjectManager = arObjectManager;
+
+    this.arSessionManager!.onInitialize(
+          showFeaturePoints: _showFeaturePoints,
+          showPlanes: _showPlanes,
+          customPlaneTexturePath: _planeTexturePath,
+          showWorldOrigin: _showWorldOrigin,
+          showAnimatedGuide: _showAnimatedGuide,
+          handleTaps: _handleTaps,
+        );
+    this.arObjectManager!.onInitialize();
+  }
+
+  void updateSessionSettings() {
+    this.arSessionManager!.onInitialize(
+          showFeaturePoints: _showFeaturePoints,
+          showPlanes: _showPlanes,
+          customPlaneTexturePath: _planeTexturePath,
+          showWorldOrigin: _showWorldOrigin,
+        );
   }
 }
